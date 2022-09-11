7c8e18685eb70d4aab789b3a49169c5be70cb0e7
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-09-11 21:44:20 +0800

    project files

---
+++ AutoLayoutPriorities/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AutoLayoutPriorities
+//
+//  Created by Jia Jie Chan on 11/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.removeFromSuperview()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
